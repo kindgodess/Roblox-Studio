a4cc5802e31ba54377fc8629a4c9e3bdb6ea3245
kindgodess <esthertg3@gmail.com>
2026-02-03 10:08:18 +0100

    Add random color changing script for block
    This script changes the color of a block randomly every half second.

---
+++ color_random.lua
@@ -0,0 +1,8 @@
+--script colores al azar
+local bloque = script.Parent
+while true do
+	wait(0.5)--Tiempo
+	bloque.BrickColor = BrickColor.Random()
+	wait(0.5)
+	bloque.BrickColor = BrickColor.Random()
+	end
